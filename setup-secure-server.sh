944e17b86471ba6f92ae78fa062f960a14c933f6
lunaweb89 <lunawebagency@gmail.com>
2025-12-05 20:34:07 +0800

    Change cron job timing for unattended upgrades

--- setup-secure-server.sh
+++ setup-secure-server.sh
@@ -169,7 +169,7 @@ EOF
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-0 4 * * * root unattended-upgrade -v >> /var/log/auto-security-updates.log 2>&1
+30 13 * * * root unattended-upgrade -v >> /var/log/auto-security-updates.log 2>&1
 EOF
 
   chmod 644 "$CRON_UPDATES"
